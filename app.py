25c28201d3be15f23116f241554e0f2959a5d3e7
Daymons <88623975+Daymons@users.noreply.github.com>
2022-11-07 18:38:53 +0100

    Update app.py - version 1!

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-import flask, random, sqlite3
+import flask, random, sqlite3, validators
 app = flask.Flask(__name__)
-rstring = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890') for i in range(8))
-
-# Configuration strings
 baseurl = "https://s.example.com"
 
 @app.route("/")
@@ -22,17 +19,49 @@ def linkredirecter(url):
         else:
             cur.execute("SELECT original FROM links WHERE short = ?", (url,))
             i = cur.fetchall()
-            k = ''.join(''.join(n) for n in i)
-            return flask.redirect(k)
-            
-@app.route("/shorten/", methods = ["POST"])
+            original_link = ''.join(''.join(n) for n in i)
+            return flask.redirect(original_link)
+     
+@app.route("/shorten/", methods = ["POST", "GET"])
 def handler():
     link = flask.request.form.get('url')
-    con = sqlite3.connect("static/short.db")
+    rstring = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890') for i in range(8))
+    con = sqlite3.connect('static/short.db')
+    con.row_factory = lambda cursor, row: row[0]
     cur = con.cursor()
-    cur.execute(f"INSERT INTO links VALUES (?, ?)", (rstring, link))
-    con.commit()
-    return f"{baseurl}/{rstring}"
+    originals = cur.execute('SELECT original FROM links').fetchall()
+    if link in originals:
+        cur.execute('SELECT short FROM links WHERE original = (?)', (link,))
+        res = cur.fetchall()
+        short = ''.join(res)
+        return f"{baseurl}/{short}"
+    else:
+        rstring = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890') for i in range(8))
+        cur.execute(f"INSERT INTO links VALUES (?, ?)", (rstring, link))
+        con.commit()
+        return f"{baseurl}/{rstring}"
+
+@app.route("/api", methods=["POST", "GET"])
+def apihandler():
+    link = flask.request.args.get('url')
+    if validators.url(link):
+        rstring = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890') for i in range(8))
+        con = sqlite3.connect('static/short.db')
+        con.row_factory = lambda cursor, row: row[0]
+        cur = con.cursor()
+        originals = cur.execute('SELECT original FROM links').fetchall()
+        if link in originals:
+            cur.execute('SELECT short FROM links WHERE original = (?)', (link,))
+            res = cur.fetchall()
+            short = ''.join(res)
+            return f"{baseurl}/{short}"
+        else:
+            rstring = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890') for i in range(8))
+            cur.execute(f"INSERT INTO links VALUES (?, ?)", (rstring, link))
+            con.commit()
+            return f"{baseurl}/{rstring}"
+    else:
+        return "Your URL is malformed. Perhaps you forgot to add 'https://' at the beggining?"
 
 if __name__ == "__main__":
     app.run()
