96d98fde26fbd72f84a1b51fe4c035a7cdaea228
Daymons <88623975+Daymons@users.noreply.github.com>
2023-01-29 14:35:48 +0100

    Initial commit & README update
    Apparently I am retarded enough to not know how to use git

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# py-shorten
-A lightweight Python URL shortener.
+# py-shorten, a lightweight Python URL shortener. 
+Made using Flask and a SQLite3 database, the program is capable of serving a URL shortener to everyone. It's very lightweight and made to run on any kind of server (even on a free EC2 instance).
+
+# How do I run it?
+You only need to clone the repository (``git clone https://github.com/Daymons/py-shorten`` if you have the Git tool installed, else just download it from that page),   
+[install Python if you havent already](https://www.python.org/downloads/) and download the required libraries with ``pip -i requirements.txt``.   
+Then, you'll only need to change the ``base_url`` variable, located at the top of the program, to match the domain you have, and you'll be ready to go!
+
+# Contact
+* Discord @ daymons.@7593   
+* Twitter @ hyperyzen5   
+* [e-mail me @ day@cvillahro.live](mailto:day@cvillahro.live)
---
+++ main.py
@@ -0,0 +1,50 @@
+import flask, sqlite3, random, validators
+app = flask.Flask(__name__)
+app.secret_key = "[qPH5,T¡3?m6c4HP}JG2hYf8xkMyOVq]xgch1PKMb02BHpV6,U1?abd!L0qr¡kZH0wjOd1CQW¿NDQd2S?WxFYhzd8gVOdpIjFYB9]zHZM¿¿y4BGDaljLpf6xt4.nz{,:"
+
+base_url = "https://your-url-here.net/"
+short_code = ''.join(random.choice("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890") for i in range(8))
+
+@app.route("/")
+def main():
+    return flask.render_template("index.html")
+
+@app.route("/shorten", methods = ["POST"])
+@app.route("/shorten/", methods = ["POST"])
+def shorten():
+    # Connect to the database and initialize the cursor.
+    con = sqlite3.connect("sqlite3.db")
+    cur = con.cursor()
+
+    # Get the HTML link and search for the link in the database.
+    html_link = flask.request.form.get("link")
+    if validators.url(html_link):
+        db_link = cur.execute("SELECT short FROM links WHERE long = ?", (html_link,)).fetchone()
+
+        # Search the database for the link. If it exists, return the already existing short code. This prevents the database being flooded with different short codes for the same long link. 
+        # If it doesn't exist, create a new code and return it.
+        if db_link == None:
+            cur.execute("INSERT INTO links VALUES (?, ?)", (html_link, short_code))
+            con.commit()
+            flask.flash("Your link is " + base_url + short_code)
+            return flask.redirect("/")
+        else:
+            flask.flash("Your link is " + base_url + ''.join(db_link))
+            return flask.redirect("/")
+    else:
+        flask.flash("The URL you inputed is not valid.", "error")
+        return flask.redirect("/")
+
+@app.route("/<code>")
+def redirect(code):
+    cur = sqlite3.connect("sqlite3.db").cursor()
+    db_link = cur.execute("SELECT long FROM links WHERE short = ?", (code,)).fetchone()
+
+    if db_link == None:
+        flask.flash("The entered URL does not exist.", "error")
+        return flask.redirect("/")
+    else:
+        return flask.redirect(''.join(db_link))
+
+if __name__ == '__main__':
+    app.run()
